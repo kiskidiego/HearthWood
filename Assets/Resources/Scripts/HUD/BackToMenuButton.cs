659c152a1e72e29f0b77299ab1676b9e6c18fc78
Diego Pérez <dperezrueda22@gmail.com>
2026-05-29 16:26:13 +0200

    fixed timescale bug

--- Assets/Resources/Scripts/HUD/BackToMenuButton.cs
+++ Assets/Resources/Scripts/HUD/BackToMenuButton.cs
@@ -4,6 +4,7 @@ public class BackToMenuButton : MonoBehaviour
 {
     public void OnBackToMenuButtonPressed()
     {
+        Time.timeScale = 1f; // Ensure time is running when going back to menu
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
     }
 }
